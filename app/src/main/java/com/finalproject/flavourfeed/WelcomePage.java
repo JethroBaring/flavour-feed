f60a07c40caee12f8efe67fda6531f66d121ce8a
Jth <jyty.dev@gmail.com>
2023-04-05 22:42:31 +0800

    Changed default app icon

---
+++ app/src/main/java/com/finalproject/flavourfeed/WelcomePage.java
@@ -0,0 +1,13 @@
+package com.finalproject.flavourfeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_page);
+    }
+}
