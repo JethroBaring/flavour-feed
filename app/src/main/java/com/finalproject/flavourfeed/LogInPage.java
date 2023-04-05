78c2791300cf7f34a4f6ea0702785c81591b74f4
Jth <jyty.dev@gmail.com>
2023-04-06 03:07:13 +0800

    Completed Sign Up Page UI

--- app/src/main/java/com/finalproject/flavourfeed/LogInPage.java
+++ app/src/main/java/com/finalproject/flavourfeed/LogInPage.java
@@ -1,5 +1,7 @@
 package com.finalproject.flavourfeed;
 
+import android.content.Intent;
+import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,9 +13,17 @@ public class LogInPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.log_in_page);
         TextView forgotPassword = findViewById(R.id.forgotPassword);
-        TextView signUp = findViewById(R.id.signUp);
+        TextView signUp = findViewById(R.id.signUpPage);
         GradientText gradientText = new GradientText();
         gradientText.setTextViewColor(forgotPassword, getResources().getColor(R.color.red), getResources().getColor(R.color.pink));
         gradientText.setTextViewColor(signUp, getResources().getColor(R.color.red), getResources().getColor(R.color.pink));
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/finalproject/flavourfeed/SignUpPage.java
+++ app/src/main/java/com/finalproject/flavourfeed/SignUpPage.java
@@ -1,5 +1,8 @@
 package com.finalproject.flavourfeed;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,16 @@ public class SignUpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_up_page);
+        TextView logInPage = findViewById(R.id.logInPage);
+        GradientText gradientText = new GradientText();
+        gradientText.setTextViewColor(logInPage,getResources().getColor(R.color.red), getResources().getColor(R.color.pink));
+
+        logInPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LogInPage.class);
+                startActivity(intent);
+            }
+        });
     }
 }
