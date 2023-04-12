0f3e99a326b7bce4ac3d2ed3f780f330478683c9
Jeth <jyty.dev@gmail.com>
2023-04-12 14:16:30 +0800

    Added Log Out feature

--- app/src/main/java/com/finalproject/flavourfeed/Fragments/ProfileFragment.java
+++ app/src/main/java/com/finalproject/flavourfeed/Fragments/ProfileFragment.java
@@ -1,19 +1,39 @@
 package com.finalproject.flavourfeed.Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.finalproject.flavourfeed.Pages.LogInPage;
 import com.finalproject.flavourfeed.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class ProfileFragment extends Fragment {
-
+    FirebaseAuth mAuth;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.profile_fragment, container, false);
+        View view = inflater.inflate(R.layout.profile_fragment, container, false);
+        mAuth = FirebaseAuth.getInstance();
+        Button btnLogOut = view.findViewById(R.id.btnLogOut);
+
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+                Intent intent = new Intent(getContext(), LogInPage.class);
+                startActivity(intent);
+            }
+        });
+
+
+        return view;
     }
 }
